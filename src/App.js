aa2e4bb6089a5bbf0a619e12492ea1a67b29ba9d
unknown <matthewpiedra1228@gmail.com>
2020-06-29 12:21:06 -0400

    Added lifecycle methods and clear button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, {Component} from 'react';
 
-class App extends Component {
+class Ticker extends Component {
   constructor() {
     super();
-
+    
     this.state = {
-       
+       ticker: 0,
     }
   }
 
+  componentDidMount() {
+     // start our ticker here
+     setInterval(() => {
+       this.setState({
+         ticker: this.state.ticker + 1,
+       });
+     }, 1000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+     // Only trigger a re-render when ticker is a multiple of 3
+     // This doesn't prevent rendering, but rather just optimizing
+     // performance
+     return nextState.ticker % 3 === 0;
+  }
+
+  clear = () => {
+     this.setState({
+       ticker: 0,
+     });
+  }
+
   render() {
+    let {ticker} = this.state;
+    
     return (
       <div>
-        
+         <p> The ticker is: {ticker} </p>
+         <button type="button" onClick={this.clear}> Reset Ticker </button>
       </div>
     ); 
   }
 }
 
-export default App;
+export default Ticker;
